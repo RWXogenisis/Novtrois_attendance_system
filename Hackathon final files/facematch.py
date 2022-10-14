e352e30eb1f28b159b8c9209d044165bd1f82edb
RWXogenisis <65536776+RWXogenisis@users.noreply.github.com>
2022-10-14 18:47:35 +0530

    Comments added

--- Hackathon final files/facematch.py
+++ Hackathon final files/facematch.py
@@ -1,9 +1,14 @@
 import face_recognition
 def face(rn, i):
+    # loading the images url
     known_image = face_recognition.load_image_file(str(rn)+".jpg")
-    unknown_image = face_recognition.load_image_file(str(i)+".jpg")            
+    unknown_image = face_recognition.load_image_file(str(i)+".jpg")        
+    
+    # encoding the images according to its properties
     biden_encoding = face_recognition.face_encodings(known_image)[0]
     unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+    
+    #comparing the images
     results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
 
     if results==[True]:
